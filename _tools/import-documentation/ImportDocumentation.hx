3efd0e394f0f0a294ed8188604e432a416e4a82e
Joshua Granick <jgranick@users.noreply.github.com>
2014-11-25 13:16:10 -0800

    Update for new documentation

--- _tools/import-documentation/ImportDocumentation.hx
+++ _tools/import-documentation/ImportDocumentation.hx
@@ -21,15 +21,41 @@ class ImportDocumentation {
 		var source = args[0];
 		var destination = args[1];
 		
-		var sourcePaths = PathHelper.readDirectory (source, [ ".git" ]);
+		var sourcePaths = PathHelper.readDirectory (source, [ ".git", "_book" ]);
 		var targetPaths = [];
 		var names = [];
 		var titles = [];
+		var summary = [];
+		
+		for (i in 0...sourcePaths.length) {
+			
+			sourcePaths[i] = sourcePaths[i].substr (source.length + 1);
+			
+		}
+		
+		try {
+			
+			var input = File.read (source + "/SUMMARY.md", false);
+			
+			summary.push ("* [Introduction](/documentation/)");
+			
+			while (true) {
+				
+				var line = input.readLine ();
+				
+				if (line != null && !StringTools.startsWith (line, "#") && StringTools.trim (line).length > 0) {
+					
+					summary.push (line);
+					
+				}
+				
+			}
+			
+		} catch (e:Dynamic) {}
 		
 		for (sourcePath in sourcePaths) {
 			
-			var path = sourcePath.substr (source.length);
-			var components = path.split ("/");
+			var components = sourcePath.split ("/");
 			var targetPath = "";
 			var name = "";
 			var title = "";
@@ -37,14 +63,10 @@ class ImportDocumentation {
 			for (i in 0...components.length - 1) {
 				
 				var component = components[i];
-				var index = component.lastIndexOf (".-");
 				
-				if (index > -1) {
+				if (component != "") {
 					
-					targetPath += component.substr (index + 2).toLowerCase () + "/";
-					
-				} else if (component != "") {
-					
+					component = StringTools.replace (component, "_", "-");
 					targetPath += component + "/";
 					
 				}
@@ -52,31 +74,49 @@ class ImportDocumentation {
 			}
 			
 			var lastComponent = components[components.length - 1];
-			var index = lastComponent.lastIndexOf (".-");
-			name = lastComponent.substr (0, lastComponent.length - 3);
 			
-			if (index > -1) {
+			if (Path.extension (lastComponent) == "md") {
 				
-				var trim = name.substr (index + 2);
-				title = StringTools.replace (trim, "-", " ");
-				targetPath += trim.toLowerCase () + "/index.md";
+				name = lastComponent.substr (0, lastComponent.length - 3);
+				
+				title = StringTools.replace (name, "_", " ");
+				name = StringTools.replace (name, "_", "-");
+				
+				if (name.toLowerCase () == "readme") {
+					
+					targetPath += "index.md";
+					
+				} else {
+					
+					targetPath += PathHelper.combine (name, "index.md");
+					
+				}
+				
+				targetPaths.push (targetPath);
+				names.push (name);
+				titles.push (title);
 				
 			} else {
 				
-				title = StringTools.replace (lastComponent, "-", " ");
-				targetPath += lastComponent;
+				targetPaths.push (null);
+				names.push (null);
+				titles.push (null);
 				
 			}
 			
-			targetPaths.push (targetPath);
-			names.push (name);
-			titles.push (title);
+		}
+		
+		for (i in 0...summary.length) {
+			
+			summary[i] = replaceLinks (summary[i], sourcePaths, targetPaths);
 			
 		}
 		
 		for (i in 0...targetPaths.length) {
 			
-			if (names[i] == "_sidebar") continue;
+			if (targetPaths[i] == null) continue;
+			if (names[i] == "SUMMARY") continue;
+			//if (names[i] == "_sidebar") continue;
 			
 			var sourcePath = sourcePaths[i];
 			var targetPath = targetPaths[i];
@@ -86,20 +126,21 @@ class ImportDocumentation {
 			Sys.println ("Copying \"" + outputPath + "\"");
 			PathHelper.mkdir (Path.directory (outputPath));
 			
-			var content = File.getContent (sourcePath);
-			content = replaceWikiLinks (content, names, targetPaths);
+			var content = File.getContent (PathHelper.combine (source, sourcePath));
+			//content = replaceWikiLinks (content, names, targetPaths);
+			content = replaceLinks (content, sourcePaths, targetPaths);
 			
 			var output = File.write (outputPath, false);
 			output.writeString ("---\n");
 			output.writeString ("layout: documentation\n");
 			output.writeString ("title: " + title + "\n");
 			output.writeString ("---\n\n");
-			output.writeString ("# " + title);
-			output.writeString (' <a href="https://github.com/openfl/openfl/wiki/${names[i]}/_edit" class="btn btn-default pull-right" role="button"><span class="glyphicon glyphicon-pencil"></span></a>');
+			//output.writeString ("# " + title);
+			output.writeString (' <a href="https://github.com/openfl/openfl-documentation/edit/master/${sourcePaths[i]}" class="btn btn-default pull-right" style="margin-top: 16px" role="button" target="_blank"><span class="glyphicon glyphicon-pencil"></span></a>');
 			output.writeString ("\n\n");
 			output.writeString (content);
 			
-			var components = sourcePath.split ("/");
+			/*var components = sourcePath.split ("/");
 			var foundSidebar = false;
 			
 			while (!foundSidebar && components.length > 1) {
@@ -120,11 +161,88 @@ class ImportDocumentation {
 					
 				}
 				
+			}*/
+			
+			output.writeString ("\n\n{% sidebar %}");
+			output.writeString ("<br />\n\n");
+			
+			var pageComponents = targetPath.split ("/");
+			pageComponents.pop ();
+			
+			for (link in summary) {
+				
+				if (link.indexOf ("<!--") > -1) continue;
+				
+				var index = link.indexOf ("](");
+				
+				if (index > -1) {
+					
+					var targetPath = link.substring (index + 3, link.lastIndexOf (")") - 1);
+					var targetComponents = targetPath.split ("/");
+					targetComponents.shift ();
+					
+					if (targetComponents.length <= 1 || pageComponents[0] == targetComponents[0]) {
+						
+						if (pageComponents.join ("/") == targetComponents.join ("/")) {
+							
+							var leftIndex = link.indexOf ("* [") + 3;
+							var rightIndex = link.indexOf ("](");
+							
+							var link = link.substring (0, leftIndex) + "__" + link.substring (leftIndex, rightIndex) + "__" + link.substr (rightIndex);
+							output.writeString (link);
+							
+						} else {
+							
+							output.writeString (link);
+							
+						}
+						
+						output.writeString ("\n");
+						
+					}
+					
+				}
+				
 			}
 			
+			output.writeString ("{% endsidebar %}");
 			output.close ();
 			
 		}
+		
+	}
+	
+	
+	private static function replaceLinks (content:String, sourcePaths:Array<String>, targetPaths:Array<String>):String {
+		
+		for (i in 0...sourcePaths.length) {
+			
+			var sourcePath = sourcePaths[i];
+			var targetPath = targetPaths[i];
+			
+			if (targetPath != null && targetPath.indexOf ("/") > -1) {
+				
+				if (StringTools.endsWith (targetPath, ".md")) {
+					
+					targetPath = targetPath.substring (0, targetPath.length - 3) + ".html";
+					
+				}
+				
+				targetPath = "/documentation/" + targetPath;
+				
+				if (StringTools.endsWith (targetPath, "/index.html")) {
+					
+					targetPath = targetPath.substring (0, targetPath.length - 10);
+					
+				}
+				
+				content = StringTools.replace (content, sourcePath, targetPath);
+					
+			}
+			
+		}
+		
+		return content;
 		
 	}
 	
