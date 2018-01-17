07397e35a24a8560922d54e7e2393e4e9397fcb3
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-17 13:24:51 -0800

    Move install/build to shell scripts

---
+++ _scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SITE_PATH=$SCRIPT_PATH/../
+
+cd $SITE_PATH/_openfl
+npm run build-docs -s
+rm -rf ../learn/npm/api
+mkdir -p ../learn/npm/api
+cp -r docs/* ../learn/npm/api
+
+cd $SITE_PATH
+bundle exec jekyll build
---
+++ _scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SITE_PATH=$SCRIPT_PATH/../
+
+cd $SITE_PATH
+bundle install
+
+git clone --depth=1 https://github.com/openfl/openfl _openfl
+cd $SITE_PATH/_openfl
+npm install typedoc
