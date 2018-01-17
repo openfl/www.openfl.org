fdbc823ed37f11b4d06ef5f1adc09c981ad3d490
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-17 13:43:23 -0800

    Add deploy as a script

---
+++ _scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SITE_PATH=$SCRIPT_PATH/../
+
+cd $SITE_PATH
+aws s3 sync _site s3://www.openfl.org --acl bucket-owner-full-control --acl public-read --size-only --cache-control max-age=604800
