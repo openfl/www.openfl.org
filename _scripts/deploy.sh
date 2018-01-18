5a07bde1b854087e31c03087e1c8439f2a9f1409
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-17 16:39:50 -0800

    Lessen caching to 60 minutes for now

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,4 +3,4 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SITE_PATH=$SCRIPT_PATH/../
 
 cd $SITE_PATH
-aws s3 sync _site s3://www.openfl.org --acl bucket-owner-full-control --acl public-read --size-only --cache-control max-age=604800 --delete
+aws s3 sync _site s3://www.openfl.org --acl bucket-owner-full-control --acl public-read --size-only --cache-control max-age=3600 --delete
