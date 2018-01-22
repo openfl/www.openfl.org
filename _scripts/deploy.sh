12bd771862a657b5698feada0a6ddb5120b91bfe
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-22 13:08:23 -0800

    Update deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -10,7 +10,8 @@ cd _deploy
 for file in $(find . -name '*.css' -or -name '*.txt' -or -name '*.js' -or -name '*.json' -or -name '*.htm' -or -name '*.html')
 do
 	##{ zopfli ${file} && mv -f ${file}.gz ${file}; }
-	gzip -t ${filename} > /dev/null 2>&1 || { gzip -9 ${filename} && mv -f ${filename}.gz ${filename}; }
+	#gzip -t ${filename} > /dev/null 2>&1 || { gzip -9 ${filename} && mv -f ${filename}.gz ${filename}; }
+	{ gzip -9 ${filename} && mv -f ${filename}.gz ${filename}; }
 done
 aws s3 sync . s3://www.openfl.org --acl bucket-owner-full-control --acl public-read --size-only --delete --exclude="*" --include="*.css" --include="*.txt" --include="*.js" --include="*.json" --include="*.htm" --include="*.html" --include="*.map" --include="*.xml" --content-encoding gzip --cache-control max-age=3600
 aws s3 sync . s3://www.openfl.org --acl bucket-owner-full-control --acl public-read --size-only --delete --include="*" --exclude="*.css" --exclude="*.txt" --exclude="*.js" --exclude="*.json" --exclude="*.htm" --exclude="*.html" --exclude="*.map" --exclude="*.xml" --cache-control max-age=864000
