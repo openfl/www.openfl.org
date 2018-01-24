5e4983fc7498b8987861da8ae44836333bc3d1a7
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-23 16:29:44 -0800

    Add dev script for replacing openfl.js

---
+++ _scripts/devbuild-openfl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SITE_PATH=$SCRIPT_PATH/../
+
+cd $SITE_PATH/node_modules/openfl
+npm run build-lib -s
+npm run build-dist:dev -s
+cp -f dist/openfl.js $SITE_PATH/_site/js/openfl.min.js
+cp -f dist/openfl.js $SITE_PATH/js/openfl.min.js
