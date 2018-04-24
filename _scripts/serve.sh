c97f1f18951f01e56da4aae0177b4a63ff9ebd9d
Joshua Granick <jgranick@users.noreply.github.com>
2018-04-24 14:57:44 -0700

    Add serve script

---
+++ _scripts/serve.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SITE_PATH=$SCRIPT_PATH/../
+
+source $SCRIPT_PATH/build.sh
+
+cd $SITE_PATH
+bundle exec jekyll serve --skip-initial-build
