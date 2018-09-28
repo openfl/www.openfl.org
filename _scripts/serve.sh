997c670cae17b435a81374600886ad7541dee276
Joshua Granick <jgranick@users.noreply.github.com>
2018-09-28 15:28:30 -0700

    Update serve script

--- _scripts/serve.sh
+++ _scripts/serve.sh
@@ -3,7 +3,8 @@ set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SITE_PATH=$SCRIPT_PATH/../
 
-source $SCRIPT_PATH/build.sh
+cd $SCRIPT_PATH
+./build.sh
 
 cd $SITE_PATH
 bundle exec jekyll serve --skip-initial-build
